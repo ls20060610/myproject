5a2e99e76355243b55367782fa26b904cad74b50
ls20060610 <a0930325385@gmail.com>
2017-03-13 13:59:26 +0800

    作業1

---
+++ src/oo/abstraction/BBQ.java
@@ -0,0 +1,19 @@
+package oo.abstraction;
+
+public class BBQ {
+	String meat;
+	String Part;
+	String Place;
+	String method;
+	int Price;
+	
+	public BBQ(String meat, String Part, String Place, String method , int Price){
+		this.meat = meat;
+		this.Part = Part;
+		this.Place = Place;
+		this.method = method;
+		this.Price = Price;
+	
+	}
+
+}
---
+++ src/oo/abstraction/BBQTester.java
@@ -0,0 +1,9 @@
+package oo.abstraction;
+
+public class BBQTester {
+
+	public static void main(String[] args) {
+		BBQ a1 = new BBQ("牛肉" , "無骨牛小排" , "澳洲", "烤到表面出水8分滿 翻面數八秒可食用" , 300);
+	}
+
+}
