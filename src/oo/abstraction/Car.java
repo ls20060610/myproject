7525e37347cd506c0ae408dc5190487cc9beede0
ls20060610 <a0930325385@gmail.com>
2017-03-14 15:58:09 +0800

    新增建構子(constructor)

--- src/oo/abstraction/Car.java
+++ src/oo/abstraction/Car.java
@@ -2,15 +2,17 @@ package oo.abstraction;
 
 public class Car {
 	String brand;
-	int cc;
 	String name;
 	String type;
+	int cc;
+	int status;
 	
-	public Car(String brand, int cc, String name, String type){
+	public Car(String brand, int cc, String name, String type, int status){
 		this.brand = brand;
 		this.cc = cc;
 		this.name = name;
 		this.type = type;
+		this.status = status;
 	}
 
 }
--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -3,7 +3,11 @@ package oo.abstraction;
 public class CarTester {
 
 	public static void main(String[] args) {
-		Car c1 = new Car("Toyota" , 1800 , "Altis", "Sedan");
+		Car c1 = new Car("Toyota" , 1800 , "Altis", "Sedan" , 100);
+		
+		Car c2 = new Car("Ford" , 2000 , "focus", "Sedan" , 100);
+		
+		Car c3 = new Car("BMW" , 1000 , "M140i", "Sedan" , 100);
 	}
 
 }
