3184ff031eb1488e7d4900d640b546db88eebd50
ls20060610 <a0930325385@gmail.com>
2017-05-16 16:24:34 +0800

    add

--- src/oo/shoping/test.java
+++ src/oo/shoping/test.java
@@ -1,25 +1,39 @@
 package oo.shoping;
 
+import java.util.ArrayList;
+
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	{
+		{
 			System.out.println("原價" + "\t" + "折扣後" + "\t" + "還原金");
-			ArrayList<Customer> list = new ArrayList();
-			list.add(new Customer(6000));
-			list.add(new SilverCustomer(8000));
-			list.add(new SilverCustomer(10000));
-			list.add(new GoldenCustomer(30000));
-			list.add(new Customer(2000));
-			list.add(new GoldenCustomer(10000));
+			ArrayList<Custumer> list = new ArrayList();
+			list.add(new Normalcust(6000));
+			list.add(new Silvercust(8000));
+			list.add(new SilverCust(10000));
+			list.add(new GoldenCust(30000));
+			list.add(new Normalcust(2000));
+			list.add(new GoldenCust(10000));
 			for (int i = 0; i < list.size(); i++) {
-				Customer cus = list.get(i);
-				cus.print();
-		
-		Custumer cus = new Custumer();
-		cus.Normalcust(100);
-		cus.Silvercust(100);
-	}
+				Custumer cus = list.get(i);
+				if (cus instanceof SilverCustomer &&
+						!(cus instanceof GoldenCustomer)){
+					System.out.print("*");
+					
+				}
+				cus.print(i);
 
+				String[] array = {"aa","bb","cc"};
+				for(String s : array){
+					System.out.print(s);
+				}
+				
+				Custumer custumer = new Custumer();
+				custumer.Normalcust(100);
+				custumer.Silvercust(100);
+			}
+
+		}
+	}
 }
