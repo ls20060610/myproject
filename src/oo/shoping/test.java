5539608980a02266c6dc5466624800bf8f49fc56
ls20060610 <a0930325385@gmail.com>
2017-05-16 16:29:46 +0800

    add

--- src/oo/shoping/test.java
+++ src/oo/shoping/test.java
@@ -15,14 +15,16 @@ public class test {
 			list.add(new GoldenCust(30000));
 			list.add(new Normalcust(2000));
 			list.add(new GoldenCust(10000));
-			for (int i = 0; i < list.size(); i++) {
-				Custumer cus = list.get(i);
-				if (cus instanceof SilverCustomer &&
+			//for (int i = 0; i < list.size(); i++) {
+			//	Custumer cus = list.get(i);
+			for(Custumer cust : list){
+				
+				if (cust instanceof SilverCustomer &&
 						!(cus instanceof GoldenCustomer)){
 					System.out.print("*");
-					
+					Silver silver = (Silver)cust;
 				}
-				cus.print(i);
+				cust.print(i);
 
 				String[] array = {"aa","bb","cc"};
 				for(String s : array){
